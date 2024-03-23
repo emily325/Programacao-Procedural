9505f76bc573c45c8e219f24a785db082c950a51
emilyrebeca1000@gmail.com <emilyrebeca1000@gmail.com>
2024-03-22 21:32:12 -0300

    lista5

---
+++ listas/lista5/exercicio03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      int altura = int.Parse(Console.ReadLine());
+    
+      int baseretangulo = int.Parse(Console.ReadLine());
+      
+      for(int linha = 0; linha < altura; linha++)
+      {
+        for(int coluna = 0; coluna < baseretangulo; coluna++)
+        {
+            System.Console.Write("*");
+        }
+        System.Console.WriteLine();
+      }
+    }
+}
---
+++ listas/lista5/exercicio04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  { 
+    int somatorio = 0;
+    do
+    {
+      int numero = int.Parse(Console.ReadLine());
+      if(numero%2==1)
+      {
+        somatorio+=numero;
+      }
+      else if(numero < 0)
+      {
+        break;
+      }
+    }
+    while(true);
+    
+      System.Console.WriteLine("o somatorio de impares eh " + somatorio);
+  }     
+}
