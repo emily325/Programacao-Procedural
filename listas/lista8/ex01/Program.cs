a221e851b48fa580ee08ff45528266bd877b458c
emilyrebeca1000@gmail.com <emilyrebeca1000@gmail.com>
2024-04-11 13:34:12 -0300

    struct

---
+++ listas/lista8/ex01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    struct Pizza()
+    {
+        public int id;
+        public string sabor;
+        public double preco;
+    }
+    static void Main(string[] args)
+    {
+       Pizza dados;
+       dados.id = int.Parse(Console.ReadLine());
+       dados.sabor = Console.ReadLine();
+       dados.preco = double.Parse(Console.ReadLine());
+       System.Console.WriteLine();
+       
+       System.Console.WriteLine(dados.id);
+       System.Console.WriteLine(dados.sabor);
+       System.Console.WriteLine(dados.preco);
+    }
+}
---
+++ listas/lista8/ex02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    struct Produto
+    {
+        public int id;
+        public string nome;
+        public double preco;
+        public bool disponivelEmEstoque;
+    }
+
+    static void Main(string[] args)
+    {
+      Produto[] produtos = new Produto[4];
+
+      for (int i = 0; i < produtos.Length; i++)
+      {
+        produtos[i].id = int.Parse(Console.ReadLine());
+        produtos[i].nome = Console.ReadLine();
+        produtos[i].preco = double.Parse(Console.ReadLine());
+        produtos[i].disponivelEmEstoque = bool.Parse(Console.ReadLine());
+      }
+      System.Console.WriteLine();
+
+      for (int i = 0; i < produtos.Length; i++)
+      {
+        if(produtos[i].disponivelEmEstoque)
+        {
+           System.Console.WriteLine( produtos[i].nome);
+           System.Console.WriteLine( produtos[i].preco);
+        }
+      }    
+    }
+}
